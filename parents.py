c9dcbedd908e87a36c6c3f16437d307a2cedfcdd
connorishall <connorishalls@gmail.com>
2021-04-21 21:44:33 -0400

    parent-class

---
+++ parents.py
@@ -0,0 +1,13 @@
+class vehicle:
+    make = ' '
+    model = ' '
+    color = ' '
+#child class of vehicle
+class motorcycle(vehicle):
+    wheels = 2
+    throttle = 'Hand throttle'
+#child class of vehicle
+class car(vehicle):
+    Gas = ' ' 
+    engine = True 
+    
