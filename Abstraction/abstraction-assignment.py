7479759b59b94b77384a90850e373adcd0da7105
connorishall <connorishalls@gmail.com>
2021-04-29 20:46:43 -0400

    Update abstraction-assignment.py

--- Abstraction/abstraction-assignment.py
+++ Abstraction/abstraction-assignment.py
@@ -9,10 +9,11 @@ class car(ABC):
         def payment(self, amount):
             pass
 
-        class DebitCardPayment():
-            def payment(self, amount):
-                print('Your purchase amount {} exceeded your $100 limit '.format(amount))
-
-obj = DebitCardPayment()
+class DebitCardPayment(car):
+    def __init__(self, amount):
+        def payment(self, amount):
+            print('Your purchase amount {} exceeded your $100 limit '.format(amount))
+            
+obj = DebitCardPayment(car)
 obj.paySlip("$400")
 obj.payment("$400")
